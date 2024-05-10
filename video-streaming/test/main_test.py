afa78d195148823da51bdc91652547d1f11292c8
adamscarlat <adamscarlat@gmail.com>
2024-05-10 11:50:35 -0400

    testing failure

--- video-streaming/test/main_test.py
+++ video-streaming/test/main_test.py
@@ -5,4 +5,4 @@ client = TestClient(app)
 
 def test_video_streaming_microservice():
     response = client.get("/video/test")
-    assert response.status_code == 200
+    assert response.status_code == 203
