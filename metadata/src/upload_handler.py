01bb24a38a52e0426ec47f17ec448cb8f608786e
adamscarlat <adamscarlat@gmail.com>
2024-05-17 08:51:01 -0400

    not saving metadata if video already exists

--- metadata/src/upload_handler.py
+++ metadata/src/upload_handler.py
@@ -12,11 +12,19 @@ class UploadedVideoMessage:
   video_path: str
 
 async def handle_uploaded_message(message: UploadedVideoMessage):
+  video_path = message["video_path"]
   async for db_client in cosmos_ops.get_database_client():
 
     collection = db_client.get_collection("videos")
+
+    document = await collection.find_one({"videoPath": video_path})
+    if document:
+      print (f"Video with path: {video_path} already exists. Ignoring...")
+      return
+
+    print (f"Saving video metadata with path: {video_path}...")
     doc_count = await collection.count_documents({})
     await collection.insert_one({
        "id": str(doc_count + 1),
-       "videoPath": message["video_path"]
+       "videoPath": video_path
     })
